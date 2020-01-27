278850068db6d57aff49e810f0d818a8831c0ff8
otniel97 <otnielperez2015@gmail.com>
2020-01-27 16:17:36 -0400

    muestra segmento con imagen en seller profile de isg_frontend

--- __manifest__.py
+++ __manifest__.py
@@ -18,13 +18,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'odoo_marketplace'],
+    'depends': ['base', 'odoo_marketplace', 'isg_frontend'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/segment_view.xml',
         'views/seller_view.xml',
+        'views/seller_view_frontend.xml',
     ],
     'installable': True,
     "application":  True,
